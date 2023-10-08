75b30f5ad5289150140926191f5e4233bf91934a
ALIAKSANDR MAROZ <115889408+Morozaaa@users.noreply.github.com>
2023-10-08 19:59:52 +0300

    lab1 for Windows

---
+++ lab1_windows/lab1_2.cpp
@@ -0,0 +1,35 @@
+﻿#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+
+void _tmain(int argc, TCHAR* argv[])
+{
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+
+    if (!CreateProcess(L"C:\\Users\\zorom\\source\\repos\\lab1_os\\x64\\Debug\\lab1_os.exe",
+        NULL,
+        NULL,
+        NULL,
+        FALSE,
+        0,
+        NULL,
+        NULL,
+        &si,
+        &pi)
+        )
+    {
+        printf("CreateProcess failed (%d).\n", GetLastError());
+        return;
+    }
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+}
---
+++ lab1_windows/lab1_os.cpp
@@ -0,0 +1,12 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+
+int main()
+{
+	while(SetConsoleCtrlHandler(NULL, false)){
+		continue;
+
+	}
+	return 0;
+}
