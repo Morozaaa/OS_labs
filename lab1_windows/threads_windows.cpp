fd65ab533603e1cdbd9940e8c9e1ff288e8ba9ec
ALIAKSANDR MAROZ <115889408+Morozaaa@users.noreply.github.com>
2023-10-22 19:15:02 +0300

    Add files via upload

---
+++ lab1_windows/threads_windows.cpp
@@ -0,0 +1,40 @@
+﻿#include<Windows.h>
+#include<processthreadsapi.h>
+#include<stdio.h>
+void reverse(){
+	while (true) {
+		char str[100];
+		scanf_s("%s", str, 100);
+		printf("%s\n", _strrev(str));
+	}
+}
+void random() {
+	while (true) {
+		printf("%f\n", (double)rand() / RAND_MAX);
+		Sleep(2000);
+	}
+}
+BOOL WINAPI CtrlHandler(_In_ DWORD dwCtrlType) {
+    if (dwCtrlType == CTRL_C_EVENT) {
+        printf("Thread ended with CTRL_C_EVENT");
+        exit(1);
+    }
+    else if (dwCtrlType == CTRL_CLOSE_EVENT) {
+        printf("Thread ended with CTRL_CLOSE_EVENT");
+        exit(2);
+    }
+    else {
+        return false;
+    }
+}
+
+int main()
+{
+    SetConsoleCtrlHandler(CtrlHandler, TRUE);
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)reverse, NULL, 0, NULL);
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)random, NULL, 0, NULL);
+    while(true){}
+    return 0;
+}
+
+
